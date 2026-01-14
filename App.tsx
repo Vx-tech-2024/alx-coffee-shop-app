c6e06e037bfcb32e110732e7e9430f643de5b497
Vx-tech <xaviervianney8@gmail.com>
2026-01-14 16:15:29 +0300

    Setting up coffe shop app

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-green-600">
+        NativeWind is working 🚀
+      </Text>
+    </View>
+  );
+}
