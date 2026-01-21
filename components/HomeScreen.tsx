213649e20f6ab432b46edb806d072e8a2feb29a6
Vx-tech <xaviervianney8@gmail.com>
2026-01-21 13:13:27 +0300

    Setting up the Home screen background and Banner Image

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,9 +1,39 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+
+const bannerImage = require("../assets/images/Banner.png");
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+
+      {/* BLACK HEADER */}
+      <View style={styles.TopBlackBackground}>
+        <Text style={styles.locationLabel}>Location</Text>
+        <Text style={styles.location}>Bilzen, Tanjungbalai</Text>
+
+        <View style={styles.searchBar}>
+          <Text style={{ color: "#999" }}>Search coffee</Text>
+        </View>
+      </View>
+
+      {/* WHITE GAP (behind banner) */}
+      <View style={styles.whitesecondbackground} />
+
+      {/* MAIN CONTENT */}
+      <View style={styles.content}>
+        <Text style={styles.sectionTitle}>All Coffee</Text>
+        {/* Coffee cards go here */}
+      </View>
+
+      {/* FLOATING PROMO BANNER */}
+      <ImageBackground
+        source={bannerImage}
+        style={styles.Banner}
+        imageStyle={styles.bannerImage}
+      >
+
+      </ImageBackground>
+
     </View>
   );
 }
@@ -11,12 +41,85 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
-    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+
+  /* BLACK HEADER */
+  TopBlackBackground: {
+    height: 280,
+    backgroundColor: "#111",
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    width: 375,
+  },
+
+  locationLabel: {
+    color: "#aaa",
+    fontSize: 12,
+  },
+
+  location: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "600",
+    marginBottom: 20,
+  },
+
+  searchBar: {
+    height: 52,
+    backgroundColor: "#222",
+    borderRadius: 16,
+    paddingHorizontal: 16,
     justifyContent: "center",
   },
-  text: {
-    color: "white",
-    fontSize: 22,
+
+  /* WHITE GAP */
+  whitesecondbackground: {
+    height: 60,
+    backgroundColor: "#fff",
+  },
+
+  /* CONTENT */
+  content: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    width: 375,
+    height: 436,
+  },
+
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+
+  /* PROMO BANNER */
+  Banner: {
+    position: "absolute",
+    top: "26%",
+    alignSelf: "center",
+    width: 327,
+    height: 140,
+    borderRadius: 16,
+    overflow: "hidden",
+    paddingTop: 211,
+  },
+
+  bannerImage: {
+    borderRadius: 20,
+  },
+
+  promoBadge: {
+    backgroundColor: "#eee",
+    alignSelf: "flex-start",
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    borderRadius: 10,
+    margin: 16,
+  },
+
+  promoText: {
+    fontWeight: "600",
   },
 });
